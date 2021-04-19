9d0ac99fea32a6b72bb5924efff216854655aaff
AagmanSuri <63902687+AagmanSuri@users.noreply.github.com>
2021-04-19 14:46:10 +0530

    addEventListener

--- app.js
+++ app.js
@@ -1,9 +1,4 @@
-console.log("Hello Script is working!");
-var a =prompt("user Name ");
-var b =prompt("pass");
-if(a=="aagman" && b=="user"){
-    alert("Login");
-}
-else{
-    alert("wrong Details");
-}
+var btnTranlate = document.querySelector("#btn-translate");
+btnTranlate.addEventListener("click", function clickEventHandeler() {
+console.log("clicked");
+})
