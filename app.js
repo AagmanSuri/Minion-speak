0e1cc109116e4202bd34c6e70c2829530d516659
AagmanSuri <63902687+AagmanSuri@users.noreply.github.com>
2021-04-19 14:20:32 +0530

    promt and alert

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log("Hello Script is working!");
+var a =prompt("user Name ");
+var b =prompt("pass");
+if(a=="aagman" && b=="user"){
+    alert("Login");
+}
+else{
+    alert("wrong Details");
+}
