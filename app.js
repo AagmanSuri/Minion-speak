5324cd719d002a51afcdea760850c31a19a42fce
AagmanSuri <63902687+AagmanSuri@users.noreply.github.com>
2021-04-22 01:08:53 +0530

    fetch .then .catch added

--- app.js
+++ app.js
@@ -2,9 +2,25 @@ var btnTranlate = document.querySelector("#btn-translate");
 var txtinput = document.querySelector("#txt-input");
 var output = document.querySelector(".output");
 
-function clickHandeler() {
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
-    output.innerText = txtinput.value + " " + "hi";
+function getTranslationURL(text) {
+    return serverURL + "?text=" + text;
+}
+
+function errorHandeler(error) {
+    console.log("error occured", error);
+    alert("Something went wrong with server ! try again after some time")
+}
+
+function clickHandeler() {
+    //  output.innerText = txtinput.value + " " + "hi";
+    var inputText = txtinput.value;
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => json.contents.translated)
+        .then(translated => output.innerText = translated)
+        .catch(errorHandeler)
 }
 
 btnTranlate.addEventListener("click", clickHandeler)
